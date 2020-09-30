9f5a678aa84799d2bd84bd8593dac7ad1c209554
Aromal Anil <aromalanilkannan@gmail.com>
2020-09-30 11:41:13 +0530

    🏀 Added useDebounce hook

---
+++ useDebounce.js
@@ -0,0 +1,31 @@
+import { useCallback } from "react";
+
+/**
+ * Debounce function generator.
+ * @param {function} inputFunction Function which is to be modified.
+ * @param {number} delay The time delay in milliseconds.
+ * @returns {function} The modified function.
+ */
+function _debounce(inputFunction, delay) {
+  let timeout;
+  return function (...args) {
+    const context = this;
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      timeout = null;
+      inputFunction.apply(context, args);
+    }, delay);
+  };
+}
+
+/**
+ * Custom hook which returns a function which is forced
+ * to wait a certain amount of time before running again.
+ *
+ * @param {function} inputFunction Function which is to be modified.
+ * @param {number} delay The time delay in milliseconds.
+ * @returns {function} The modified function.
+ */
+const useDebounce = (inputFunction, delay) =>  useCallback(_debounce(inputFunction, delay), []);
+
+export default useDebounce;
