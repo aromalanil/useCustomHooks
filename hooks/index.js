624f2888db74d6254d647bfea4130770a2a3d29b
Aromal Anil <aromalanilkannan@gmail.com>
2020-10-02 11:49:31 +0530

    🐞bug : fixed npm import bug

--- hooks/index.js
+++
@@ -1,3 +0,0 @@
-export { default as useDebounce } from "./useDebounce";
-export { default as useGeoLocation } from "./useGeoLocation";
-export { default as useLocalStorage } from "./useLocalStorage";
---
+++ index.js
@@ -0,0 +1,3 @@
+export { default as useDebounce } from "./hooks/useDebounce";
+export { default as useGeoLocation } from "./hooks/useGeoLocation";
+export { default as useLocalStorage } from "./hooks/useLocalStorage";
