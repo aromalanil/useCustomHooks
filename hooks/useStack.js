302577056b99c4ab4a5be1cdc62724c3f10884ce
Aromal Anil <aromalanilkannan@gmail.com>
2020-10-03 15:23:32 +0530

    📚 Added useStack hook

---
+++ hooks/useStack.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+/**
+ * Custom hook for creating and managing Stack.
+ *
+ * @param {Array} initialValue Initial value of the stack.
+ * @return {Array} Array containing the stack, push and pop functions respectively.
+ */
+const useStack = (initialValue) => {
+  const [stack, setStack] = useState(initialValue);
+
+  /**
+   *
+   * Function to add a value to the top of the stack.
+   *
+   * @param {any} value Value to be added to the stack.
+   * @returns {undefined} This function returns nothing.
+   */
+  const push = (value) => {
+    setStack([...stack, value]);
+  };
+
+  /**
+   *
+   * Function to remove the value from the top of the stack.
+   *
+   * @returns {any} The value removed from the stack.
+   */
+  const pop = () => {
+    if (stack.length > 0) {
+      let newStack = [...stack];
+      const poppedValue = newStack.pop();
+      setStack(newStack);
+      return poppedValue;
+    } else return undefined;
+  };
+
+  return [stack, push, pop];
+};
+
+export default useStack;
