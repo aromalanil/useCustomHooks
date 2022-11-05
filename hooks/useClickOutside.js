76cfde73737595699f65d7773698814c28438f65
imevanc <evan.charalampidis@gmail.com>
2022-11-05 14:03:43 +0000

    removed useCapture, removed the initial value and setting to true in the ref updateef

--- hooks/useClickOutside.js
+++ hooks/useClickOutside.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
 
 /**
  * @typedef {Object} ElementRef
  * @property {myRef} - reference to the DOM element
  * @property {wasClicked} - state
+ * @property {setWasClicked} - setter of the state
  */
 
 /**
@@ -11,27 +12,27 @@ import React from 'react';
  * Custom hook which return an object with the refence to the DOM element,
  * the state and the setter of the state.
  * @param {boolean} initialValue Initial value of the state.
- * @return {ElementRef} Object containing the coordinates of the mouse pointer.
+ * @return {ElementRef} Object containing the ref of the DOM element, the click state and its setter.
  */
-const useClickOutside = (initialValue) => {
-  const [wasClicked, setWasClicked] = React.useState(initialValue);
+const useClickOutside = () => {
+  const [wasClicked, setWasClicked] = React.useState(false);
   const myRef = React.useRef(null);
 
   const clickOutside = (event) => {
     if (myRef.current && !myRef.current.contains(event.target)) {
-      setWasClicked(false);
+      setWasClicked(true);
     }
   };
 
   React.useEffect(() => {
     // Event listener to track the component activity.
-    document.addEventListener('click', clickOutside, true);
+    document.addEventListener("click", clickOutside);
     return () => {
-      document.removeEventListener('click', clickOutside, true);
+      document.removeEventListener("click", clickOutside);
     };
   }, []);
 
-  return { myRef, wasClicked };
+  return { myRef, wasClicked, setWasClicked };
 };
 
 export default useClickOutside;
